af8031e2a8136d05ef0ec595587d46875b92b213
Pineapple217 <rosierjules@gmail.com>
2024-03-15 09:22:19 +0100

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"Stage-2024-dashboard/internal/server"
+	"fmt"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(fmt.Sprintf("cannot start server: %s", err))
+	}
+}
