e4e52d6d7cb6ad0c00cf335c224ad6de2b73093e
JulesRosier <jules.rosier@student.hogent.be>
2024-04-10 14:53:44 +0200

    consumer group env

--- pkg/kafka/connection.go
+++ pkg/kafka/connection.go
@@ -12,13 +12,26 @@ import (
 
 func GetClient() *kgo.Client {
 	seed := os.Getenv("SEED_BROKER")
+	group := os.Getenv("CONSUMER_GROUP")
 
 	slog.Info("Starting kafka client", "seedbrokers", seed)
-	cl, err := kgo.NewClient(
-		kgo.SeedBrokers(seed),
-		kgo.ConsumeTopics("^[A-Za-z].*$"),
-		kgo.ConsumeRegex(),
-	)
+
+	var cl *kgo.Client
+	var err error
+	if group == "" {
+		cl, err = kgo.NewClient(
+			kgo.SeedBrokers(seed),
+			kgo.ConsumeTopics("^[A-Za-z].*$"),
+			kgo.ConsumeRegex(),
+		)
+	} else {
+		cl, err = kgo.NewClient(
+			kgo.SeedBrokers(seed),
+			kgo.ConsumeTopics("^[A-Za-z].*$"),
+			kgo.ConsumeRegex(),
+			kgo.ConsumerGroup(group),
+		)
+	}
 	if err != nil {
 		panic(err)
 	}
