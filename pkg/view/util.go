1579fd84f865f834c7aaec52c23548218e3604f0
JulesRosier <jules.rosier@student.hogent.be>
2024-04-17 11:08:12 +0200

    Add mutex to caser for thread safety

--- pkg/view/util.go
+++ pkg/view/util.go
@@ -2,13 +2,18 @@ package view
 
 import (
 	"strings"
+	"sync"
 
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 )
 
 var caser = cases.Title(language.AmericanEnglish)
+var caserM = sync.Mutex{}
 
 func prittyName(s string) string {
-	return caser.String(strings.ReplaceAll(s, "_", " "))
+	caserM.Lock()
+	r := caser.String(strings.ReplaceAll(s, "_", " "))
+	caserM.Unlock()
+	return r
 }
